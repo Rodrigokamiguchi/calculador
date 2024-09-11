00f8ac552c201f77268eaf0ab2e2f0bacf4b7be5
Rodrigo Kamiguchi <rodrigokamiguchi@gmail.com>
2024-09-11 10:18:15 -0300

    atualizado a calculadora

--- calculadora.py
+++ calculadora.py
@@ -24,6 +24,33 @@ def media_aritmetica(numeros):
     if len(numeros) == 0:
         return "Erro, sem números para calcular a média"
     return sum(numeros) / len(numeros)
+def numero_maior(a, b):
+    if a > b:
+        return f"O maior numero é: {a:.2f}\nO menor numero é: {b:.2f}"
+    elif b > a:
+        return f"O maior numero é: {b:.2f}\nO menor numero é: {a:.2f}"
+    else:
+        return "Os numeros são iguais"
+def idade():
+    ano_atual = int(input("digite o ano atual: "))
+    ano_nascimento = int(input("digite o ano de nascimento: "))
+
+    idade = ano_atual - ano_nascimento
+    print(f"Sua idade é: {idade:.2f}anos")
+def area_do_triangulo():
+    largura = float(input("Digite a largura: "))
+    comprimento = float(input("Digite o comprimento: "))
+    
+    area = largura * comprimento
+    print(f"A area do tringulo é: {area:.2f}cm")
+def antecessor_sucessor():
+    numero = int(input("Digite um numero: "))
+
+    antecessor = numero - 1
+    sucessor = numero + 1 
+    
+    print(f"O antecessor é: {antecessor:.2f}")
+    print(f"O sucessor é: {sucessor:.2f}")
 
 def mostrar_menu():
     print("Escolha uma operação")
@@ -34,16 +61,20 @@ def mostrar_menu():
     print("5, Porcetagem")
     print("6, Resto da Divisão")
     print("7, Media Aritmetica")
+    print("8, Maior e Menor Numero")
+    print("9, Calcular a idade")
+    print("10, Calcular a area do triangulo")
+    print("11, Antecessor e sucessor")
 
 
 while True:
     mostrar_menu()
-    escolha = input("Digite o número da operação (1,2,3,4,5,6) ou sair para encerrar: ")
+    escolha = input("Digite o número da operação (1,2,3,4,5,6...) ou sair para encerrar: ")
 
     if escolha.lower() == 'sair':
         print(f"Até mais!")
         break
-    if escolha in ['1', '2', '3', '4', '5', '6', '7']:
+    if escolha in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']:
         try:
             if escolha == '5':
                 num3 = float(input("Digite o número: "))
@@ -63,6 +94,19 @@ while True:
                     numeros.append(numero)
                 resultado = media_aritmetica(numeros)
                 print(f"Media Aritmetica: {resultado}")
+            elif escolha == '8':
+                num1 = float(input("Digite o primeiro número: "))
+                num2 = float(input("Digite o segundo número: "))
+
+                resultado = numero_maior(num1, num2)
+                print(resultado)
+            elif escolha == '9':
+                idade()
+            elif escolha == '10':
+                area_do_triangulo()
+            elif escolha == '11':
+                antecessor_sucessor()
+
             else:
                 num1 = float(input("Digite o primeiro numero: "))
                 num2 = float(input("Digite o segundo numero: "))
